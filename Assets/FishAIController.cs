96349e63a68c731abf65de71bffbafb9f0456cc3
Luke Walter <lwalter5667@gmail.com>
2024-04-21 20:52:28 -0400

    Added randomization to make movements more natural

--- Assets/FishAIController.cs
+++ Assets/FishAIController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -54,19 +51,41 @@ public class FishAIController : MonoBehaviour {
     } // Update
 
     private void SetNextWaypoint() {
-        Debug.Log($"Set next waypoint | Angle: {transform.rotation.y}");
-        Vector3 diff = lookaheadDistance * new Vector3(
-            Mathf.Sin((1.0f / Mathf.Rad2Deg) * transform.eulerAngles.y), 
-            0, 
-            Mathf.Cos((1.0f / Mathf.Rad2Deg) * transform.eulerAngles.y)
-        );
-        Vector3 dest = transform.position + diff;
 
-        // TODO: Add terrain avoidance/boundaries
+        Vector2 center = new Vector2((xRange.x + xRange.y) / 2, (zRange.x + zRange.y) / 2);
+        float distanceRatioCenterX = (center.x - transform.position.x) / (xRange.y - xRange.x);
+        float distanceRatioCenterZ = (center.y - transform.position.z) / (zRange.y - zRange.x);
+        float distanceRatioFromCenter = new Vector2(distanceRatioCenterX, distanceRatioCenterZ).magnitude;
+
+        int directionChangeChance = (int)(80 * (1.0f - distanceRatioFromCenter));
+        bool directionChange = ((int) Random.Range(0, 99)) >= directionChangeChance;
+
+        int angle = (directionChange) ? ((int) Random.Range(0, 359)) : 0;
+
+        // Debug.Log($"Set next waypoint | Angle: {transform.rotation.y}");
+        Vector3 dest = GetDestination(angle);
+        int direction = (((int) Random.Range(0, 99)) >= 50) ? 1 : -1;
+
+        while (Physics.CheckSphere(dest, transform.position.y - 0.1f)) {
+            angle += 5 * direction;
+            dest = GetDestination(angle);
+
+        } // while
 
         agent.SetDestination(dest);
-        Debug.Log($"Curr: {transform.position} | Diff: {diff} | Dest: {dest}");
+        // Debug.Log($"Curr: {transform.position} | Dest: {dest}");
 
     } // SetNextWaypoint
+
+    private Vector3 GetDestination(int angleInDegrees) {
+
+        Vector3 diff = lookaheadDistance * new Vector3(
+            Mathf.Sin((1.0f / Mathf.Rad2Deg) * (transform.eulerAngles.y + angleInDegrees)), 
+            0, 
+            Mathf.Cos((1.0f / Mathf.Rad2Deg) * (transform.eulerAngles.y + angleInDegrees))
+        );
+        return transform.position + diff;
+        
+    } // GetDestination
 
 } // FishAIController
